dc6e0ccdcf12d29df5799b28ad080ff275c09c15
Dolly Jain <dolijain2690@gmail.com>
2025-10-03 22:31:29 +0530

    added changes

---
+++ oops/op.py
@@ -0,0 +1,16 @@
+class Point:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+    def sum(self,p):
+     return Point((self.x+p.x),(self.y+p.y))    
+    def print_point(self):
+        print(f"X is {self.x} and Y is {self.y}")
+    def __add__(self,p):
+     return Point((self.x+p.x),(self.y+p.y))    
+
+p1=Point(3,2)
+p2=Point(7,6)   
+#p=p1.sum(p2)#returns a new point
+p=p1+p2
+p.print_point()
